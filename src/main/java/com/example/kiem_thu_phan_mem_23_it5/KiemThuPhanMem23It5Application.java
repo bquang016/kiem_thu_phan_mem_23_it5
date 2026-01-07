0f67f5577da1aea037992e5f1d29dab4e6c1b7b6
bquang016 <buidangquangmtp@gmail.com>
2026-01-07 15:31:02 +0700

    first commit

---
+++ src/main/java/com/example/kiem_thu_phan_mem_23_it5/KiemThuPhanMem23It5Application.java
@@ -0,0 +1,13 @@
+package com.example.kiem_thu_phan_mem_23_it5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KiemThuPhanMem23It5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KiemThuPhanMem23It5Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kiem_thu_phan_mem_23_it5/KiemThuPhanMem23It5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kiem_thu_phan_mem_23_it5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KiemThuPhanMem23It5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
